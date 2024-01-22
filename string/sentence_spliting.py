9be14d6e268cee976129a5e947ddcd9e74815234
akshat <akshat@plutos.one>
2024-01-23 01:28:31 +0530

    string problem resolved

---
+++ string/sentence_spliting.py
@@ -0,0 +1,8 @@
+"""Create a variable to store the string "Earth revolves around the sun"
+1. Print "revolves" using slice operator
+2. Print "sun" using negative index"""
+
+str = "Earth revolves around the sun"
+
+print(str[6:14])
+print(str[-3:])
