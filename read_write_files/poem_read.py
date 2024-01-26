6a9c2d58fcd586c2fcbd83d44bda25b07da0c1ba
akshat <akshat@plutos.one>
2024-01-27 00:09:16 +0530

    word count of a file

--- read_write_files/poem_read.py
+++ read_write_files/poem_read.py
@@ -2,7 +2,20 @@
 poem.txt contains famous poem "Road not taken" by poet Robert Frost. You have to read this
  file in your python program and find out words with maximum occurance.
 """
+import time
 
+word_count = {}
 with open("poem.txt","r") as poem:
     for line in poem:
-        print(line)
+        line.replace("\n","")
+        words = line.split(" ")
+        for word in words:
+            if word in word_count:
+                word_count[word] +=1
+            else:
+                word_count[word] = 1
+    poem.close()
+
+word_occurances = list(word_count.values())
+max_count = max(word_occurances)
+print(max_count)
