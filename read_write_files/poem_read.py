6e0a00b706b57e0276196afa45ad27d7671bff50
akshat <akshat@plutos.one>
2024-01-27 00:14:27 +0530

    word count program of a file added to the repo

--- read_write_files/poem_read.py
+++ read_write_files/poem_read.py
@@ -1,1 +1,17 @@
-import file
+import time
+
+word_count = {}
+with open("poem.txt","r") as poem:
+    for line in poem:
+        line.replace("\n","")
+        words = line.split(" ")
+        for word in words:
+            if word in word_count:
+                word_count[word] +=1
+            else:
+                word_count[word] = 1
+    poem.close()
+
+word_occurances = list(word_count.values())
+max_count = max(word_occurances)
+print(max_count)
