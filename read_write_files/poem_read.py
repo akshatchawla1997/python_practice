933f767333b37028ff3ae4ee19a80e83bbed0065
akshat <akshat@plutos.one>
2024-01-26 23:48:06 +0530

    poem.txt heading added

--- read_write_files/poem_read.py
+++ read_write_files/poem_read.py
@@ -1,1 +1,8 @@
-import file
+"""
+poem.txt contains famous poem "Road not taken" by poet Robert Frost. You have to read this
+ file in your python program and find out words with maximum occurance.
+"""
+
+with open("poem.txt","r") as poem:
+    for line in poem:
+        print(line)
