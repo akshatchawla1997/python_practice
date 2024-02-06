1c890f92eaa52eb7ab916a482857d55f188b6d90
akshat <akshat@plutos.one>
2024-02-06 10:30:55 +0530

    dot and scalar product of vector

--- numpy/vectorbasic.py
+++ numpy/vectorbasic.py
@@ -4,6 +4,29 @@
 
 import numpy as np
 
-vector = np.linspace(5,50,10)
+vector1 = np.linspace(5,50,10)
 
-print(vector)
+print(vector1)
+
+"""
+Vector Dot Product 
+In mathematics, the dot product or scalar product is an algebraic operation that takes two equal-length 
+sequences of numbers and returns a single number. 
+For this we will use dot method.
+"""
+
+vector2 = np.linspace(10,100,10)
+
+dotproduct = vector1.dot(vector2)
+
+print(f"dot product of 2 vectors are {dotproduct}")
+
+"""
+Vector-Scalar Multiplication 
+Multiplying a vector by a scalar is called scalar multiplication. To perform scalar multiplication, 
+we need to multiply the scalar by each component of the vector.
+"""
+
+scalar_multiplication = vector1 * vector2
+
+print(f"scalar multiplication of 2 vectors are {scalar_multiplication}")
